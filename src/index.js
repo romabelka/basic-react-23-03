0f02948cadb7b4f9c000c412fc3dec7bb9fcc8a2
Roman Iakobchuk <kaboom.ua@gmail.com>
2018-03-29 19:14:50 +0300

    fix missing css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './app';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './app'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
