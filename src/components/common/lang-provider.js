d067b8519599df636bc75a8f72e5a3dadf26832f
Roman Iakobchuk <kaboom.ua@gmail.com>
2018-04-26 19:40:31 +0300

    add new context api

--- src/components/common/lang-provider.js
+++ src/components/common/lang-provider.js
@@ -1,27 +1,18 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import dictionaries from '../../dictionaries'
+import { Provider } from '../lang-context'
 
 class LangProvider extends Component {
     static propTypes = {
         language: PropTypes.string
     };
 
-    static childContextTypes = {
-        dictionary: PropTypes.object
-    }
-
-    getChildContext() {
-        return {
-            dictionary: dictionaries[this.props.language]
-        }
-    }
-
     render() {
         return (
-            <div>
+            <Provider value = {dictionaries[this.props.language]}>
                 {this.props.children}
-            </div>
+            </Provider>
         )
     }
 }
--- src/components/common/localized-text.js
+++ src/components/common/localized-text.js
@@ -1,19 +1,24 @@
+import React from 'react'
 import PropTypes from 'prop-types'
+import { Consumer } from '../lang-context'
 
-function LocalizedText(props, context) {
+function LocalizedText(props) {
     if (typeof props.children !== 'string') {
         console.warn('string child expected')
         return props.children
     }
-    return context.dictionary[props.children] || props.children
+
+    return (
+        <Consumer>
+            {
+                dictionary => dictionary[props.children] || props.children
+            }
+        </Consumer>
+    )
 }
 
 LocalizedText.propTypes = {
     children: PropTypes.string
-}
-
-LocalizedText.contextTypes = {
-    dictionary: PropTypes.object.isRequired
 }
 
 export default LocalizedText
